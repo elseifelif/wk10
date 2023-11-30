9c39a3c25bfb825e28253af7ba53b23c75a85d73
elseifelif <141683694+elseifelif@users.noreply.github.com>
2023-11-30 07:21:54 +0300

    first commit

---
+++ SQL_home_work_11.sql
@@ -0,0 +1,27 @@
+-- Ödev 11
+
+SELECT first_name FROM actor
+UNION
+SELECT first_name FROM customer;
+
+SELECT first_name FROM actor
+INTERSECT
+SELECT first_name FROM customer;
+
+SELECT first_name FROM actor
+EXCEPT
+SELECT first_name FROM customer;
+
+-- İlk 3 sorgunun tekrar eden veriler için uygulanması:
+
+SELECT first_name FROM actor
+UNION ALL
+SELECT first_name FROM customer;
+
+SELECT first_name FROM actor
+INTERSECT ALL
+SELECT first_name FROM customer;
+
+SELECT first_name FROM actor
+EXCEPT ALL
+SELECT first_name FROM customer;
