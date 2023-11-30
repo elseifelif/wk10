5068082c2178fb45b6b9e5f9324ecfc1c44093dc
elseifelif <141683694+elseifelif@users.noreply.github.com>
2023-11-30 08:16:40 +0300

    first commit

---
+++ SQL_home_work_12.sql
@@ -0,0 +1,19 @@
+-- Ödev 12
+
+SELECT COUNT(*) 
+FROM film 
+WHERE length > (SELECT AVG(length) FROM film);
+
+SELECT COUNT(*) 
+FROM film 
+WHERE rental_rate = (SELECT MAX(rental_rate) FROM film);
+
+SELECT * 
+FROM film 
+WHERE rental_rate = (SELECT MIN(rental_rate) FROM film) 
+AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
+
+SELECT customer_id, COUNT(payment_id) AS total_payments 
+FROM payment 
+GROUP BY customer_id 
+ORDER BY total_payments DESC;
